df424940f7269db1f6341caeece7983def43dc37
Luca D'Onofrio <lucadonofrio097@gmail.com>
2024-05-15 14:36:34 -0300

    added life counters and its functionality

--- src/app/life-counter/life-counter.component.ts
+++ src/app/life-counter/life-counter.component.ts
@@ -12,4 +12,30 @@ export class LifeCounterComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  topImages = [
+    { src: "../../assets/pictures/air_element.jpg"},
+    { src: "../../assets/pictures/earth_element.jpg"},
+    { src: "../../assets/pictures/fire_element.jpg" },
+    { src: "../../assets/pictures/water_element.jpg" }
+  ];
+  bottomImages = [
+    { src: "../../assets/pictures/air_element.jpg"},
+    { src: "../../assets/pictures/earth_element.jpg"},
+    { src: "../../assets/pictures/fire_element.jpg" },
+    { src: "../../assets/pictures/water_element.jpg" }
+  ];
+  counts = [0, 0, 0, 0, 0, 0, 0, 0];
+
+  handleClick(index: number, event: MouseEvent) {
+    const imageElement = event.target as HTMLElement;
+    const rect = imageElement.getBoundingClientRect();
+    const y = event.clientY - rect.top;
+    
+    if (y < rect.height / 2) {
+      this.counts[index]++;
+    } else {
+      this.counts[index]--;
+    }
+  }
+
 }

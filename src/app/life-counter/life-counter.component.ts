e7fadb356fd855c6802d62df8dec65f1ab329dcb
Luca D'Onofrio <lucadonofrio097@gmail.com>
2024-05-16 10:11:11 -0300

    rollback

--- src/app/life-counter/life-counter.component.ts
+++ src/app/life-counter/life-counter.component.ts
@@ -17,14 +17,14 @@ export class LifeCounterComponent implements OnInit {
     { src: "/sorceryLife/assets/pictures/earth_element.jpg"},
     { src: "/sorceryLife/assets/pictures/fire_element.jpg" },
     { src: "/sorceryLife/assets/pictures/water_element.jpg" },
-    { src: "assets/pictures/mana.png"}
+    { src: "/sorceryLife/assets/pictures/mana.png"}
   ];
   bottomImages = [
     { src: "/sorceryLife/assets/pictures/air_element.jpg"},
     { src: "/sorceryLife/assets/pictures/earth_element.jpg"},
     { src: "/sorceryLife/assets/pictures/fire_element.jpg" },
     { src: "/sorceryLife/assets/pictures/water_element.jpg" },
-    { src: "assets/pictures/mana.png"}
+    { src: "/sorceryLife/assets/pictures/mana.png"}
   ];
   counts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
   topLife:number = 20;
